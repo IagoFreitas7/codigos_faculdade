20507a37571a062e6cd80470edc6a97d51584a8d
Iago Freitas <92889627+IagoFreitas7@users.noreply.github.com>
2026-09-21 16:01:41 -0300

    first commit

--- LP1/Lab - Sistema de Gerenciamento de Voos/linha_aerea_iagola.cpp
+++ LP1/Lab - Sistema de Gerenciamento de Voos/linha_aerea_iagola.cpp
@@ -18,7 +18,7 @@ struct Voo{
 
 
 void menu();
-void cadastrarVoo(Voo LA[], int *i);
+void cadastrarVoo(Voo LA[]);
 void consultarVoo(Voo LA[], int num);
 void reservarPassagem(Voo LA[], int num);
 void cancelarReserva(Voo LA[], int num);
@@ -28,23 +28,32 @@ float calcularFaturamento(Voo LA[], int num); // faturamento = qnt de passageiro
 void limpar();
 void validar(int &var);
 void gravarVoo(string texto);
+void lerArquivo();
+void carregaVetor(Voo LA[]);
+string limparTexto(string texto);
+int contarVoos(Voo LA[]);
+void atualizaArquivo(Voo LA[]);
+void deletarVoo(Voo LA[], int num);
 
 
 int main() {
-  Voo LA[TAM];
+  Voo LA[TAM] = {};
   int id = 0;
   int entrada;
   int num;
+  carregaVetor(LA);
 
   do{
     menu();
+    id = contarVoos(LA);
     cin >> entrada;
 
     switch (entrada){
       case 1:
-        cadastrarVoo(LA, &id);
-        if (id < TAM) 
+        if (id < TAM){
+          cadastrarVoo(LA);
           id++;
+        } 
         else 
           cout << "Numero maximo de voos registrados" << endl;
         break;
@@ -84,7 +93,15 @@ int main() {
         calcularFaturamento(LA, num);
         limpar();
         break;
-      case 0:
+      case 8:
+        lerArquivo();
+        limpar();
+        break;
+      case 9:
+        cout << "Informe o numero do voo:" << endl;
+        cin >> num;
+        deletarVoo(LA, num);
+        limpar();
         break;
     }
   }
@@ -94,6 +111,7 @@ int main() {
 }
 
 
+//printa opcoes para o usuario
 void menu(){
   cout << "COMPANHIA AEREA" << endl;
   cout << "1 - Cadastrar voo" << endl;
@@ -103,40 +121,47 @@ void menu(){
   cout << "5 - Alterar preco da passagem" << endl;
   cout << "6 - Consultar assentos disponiveis" << endl;
   cout << "7 - Consultar faturamento estimado" << endl;
+  cout << "8 - Exibir todos os voos" << endl;
+  cout << "9 - Exclui um voo" << endl;
   cout << "0 - Encerrar" << endl << endl;
   cout << "Escolha uma opcao:" << endl;
 }
 
 
-void cadastrarVoo (Voo LA[], int *i){
+//falta: validar tipos das entradas
+//colocar regra para numero do voo
+void cadastrarVoo(Voo L[]){
+  Voo LA;
   string texto;
+  //caracteres
   string aspa = "\"", csv = ",";
 
   cout << "Digite o numero do voo" << endl;
-  cin >> LA[*i].num;
-  //validar(LA[*i].num);
+  cin >> LA.num;
+  //validar(LA.num);
   cin.ignore();
 
   cout << "Digite a origem do voo" << endl;
-  getline(cin, LA[*i].origem);
+  getline(cin, LA.origem);
 
   cout << "Digite o destino do voo" << endl;
-  getline(cin, LA[*i].destino);
+  getline(cin, LA.destino);
 
   cout << "Digite a capacidade maxima da aeronave" << endl;
-  cin >> LA[*i].capacidade;
+  cin >> LA.capacidade;
 
   //quantidade inicial de passageiros
-  LA[*i].qnt = 0;
+  LA.qnt = 0;
 
   cout << "Digite o preco da passagem" << endl;
-  cin >> LA[*i].preco;
+  cin >> LA.preco;
 
-  texto = aspa + to_string(LA[*i].num) + aspa + csv + aspa + LA[*i].origem + aspa + csv +
-  aspa + LA[*i].destino + aspa + csv + aspa + to_string(LA[*i].capacidade) + aspa + csv +
-  aspa + to_string(LA[*i].qnt) + aspa + csv + aspa + to_string(LA[*i].preco) + aspa;
+  texto = aspa + to_string(LA.num) + aspa + csv + aspa + LA.origem + aspa + csv +
+  aspa + LA.destino + aspa + csv + aspa + to_string(LA.capacidade) + aspa + csv +
+  aspa + to_string(LA.qnt) + aspa + csv + aspa + to_string(LA.preco) + aspa;
 
   gravarVoo(texto);
+  carregaVetor(L);
 }
 
 
@@ -167,6 +192,7 @@ void reservarPassagem(Voo LA[], int num){
       if(LA[i].qnt < LA[i].capacidade){
         LA[i].qnt++;
         cout << "Sua reserva foi realizada com sucesso" << endl;
+        atualizaArquivo(LA);
       }
       else
         cout << "Nao ha vagas disponiveis no voo" << endl;
@@ -185,6 +211,7 @@ void cancelarReserva(Voo LA[], int num){
       if(LA[i].qnt > 0){
         LA[i].qnt--;
         cout << "Sua reserva foi cancelada com sucesso" << endl;
+        atualizaArquivo(LA);
       }
       else
         cout << "Nao existe nenhuma reserva nesse voo" << endl;
@@ -203,6 +230,7 @@ void alterarPreco(Voo LA[], int num){
       cout << "Digite o novo preco da passagem" << endl;
       cin >> LA[i].preco; 
       cout << "Preco da passagem alterado" << endl;
+      atualizaArquivo(LA);
       return;
     }
   }
@@ -256,11 +284,11 @@ void validar(int &var){
   cout << "Entrada invalida. Digite novamente: ";
   cin.clear();
   cin.ignore(10000, '\n'); 
-}
+  }
 }
 
 
-// file 
+// Guarda o voo cadastrado em um arquivo csv
 void gravarVoo(string texto){
   // cria ou abre o arquivo no modo append
   ofstream file("meusvoos.csv", ios::app);
@@ -271,22 +299,136 @@ void gravarVoo(string texto){
   } else {
       cout << "Erro ao abrir o arquivo." << endl;
   }
+
 }
 
 
-// void lerArquivo(){
-//   // cria ou abre o arquivo no modo de leitura
-//     ofstream file("meusvoos.csv", ios::app);
+//le todos os voos do arquivo e printa
+void lerArquivo(){
+    // cria ou abre o arquivo no modo de leitura
+    ifstream file("meusvoos.csv");
+    string texto;
+    int i = 0;
 
-//     if (file.is_open()) {
-//         file << texto << endl;
-//         file.close();
-//     } else {
-//         cout << "Erro ao abrir o arquivo." << endl;
-//     }
-// }
+    if (file.is_open()) {
+        while (!file.eof()){
+          if (i < 5) {
+            getline(file, texto, ',');
+          }
+          else {
+            getline(file, texto);
+          }
+
+          switch (i){
+          case 0:
+            cout << "Numero do voo: " << limparTexto(texto) << endl;
+            break;
+          case 1:
+            cout << "Origem do voo: " << limparTexto(texto) << endl;
+            break;
+          case 2:
+            cout << "Destino do voo: " << limparTexto(texto) << endl;
+            break;
+          case 3:
+            cout << "Capacidade da aeronave: " << limparTexto(texto) << endl;
+            break;
+          case 4:
+            cout << "Quantidade de passageiros: " << limparTexto(texto) << endl;
+            break;
+          case 5:
+            cout << "Preco da passagem: " << limparTexto(texto) << endl;
+            break;  
+        }
+        i++;
+        if(i==6){
+          cout << endl;
+          i = 0;
+        }
+      }
+        file.close();        
+    }
+    else {
+        cout << "Erro ao abrir o arquivo." << endl;
+    }
+}
 
 
+//preenche o vetor com dados do arquivo
+void carregaVetor(Voo LA[]){
+    for (int k = 0; k < TAM; k++) {
+      LA[k] = Voo{};
+    }
+
+    ifstream file("meusvoos.csv");
+
+    if (!file.is_open()) {
+        cout << "Erro ao abrir o arquivo." << endl;
+        return;
+    }
+
+    string texto, textoLimpo = "";
+    int i = 0, j = 0;
+
+    while (!file.eof()){
+        if (i < 5) {
+            getline(file, texto, ',');
+        }
+        else {
+            getline(file, texto);
+        }
+
+        if (file.fail()) {
+            break;
+        }
+      
+        textoLimpo = limparTexto(texto);
+        switch (i){
+          case 0:
+            LA[j].num = stoi(textoLimpo);       
+            break;
+          case 1:
+            LA[j].origem = textoLimpo;
+            break;
+          case 2:
+            LA[j].destino = textoLimpo;
+            break;
+          case 3:
+            LA[j].capacidade = stoi(textoLimpo);
+            break;
+          case 4:
+            LA[j].qnt = stoi(textoLimpo);
+            break;
+          case 5:
+            LA[j].preco = stof(textoLimpo);
+            j++;
+            break;  
+        }
+        i++;
+
+        if(i == 6)
+          i = 0;
+        if(j >= TAM)
+          break;
+      }
+
+    file.close();        
+}
+    
+
+//funcao pra capturar texto do csv com aspas e devolve apenas o conteudo
+string limparTexto(string texto){
+  string textoLimpo;
+
+    for (int i = 0; i < texto.length(); i++) {
+        if (texto[i] != '\"'&& (texto[i] != '\n' && texto[i] != '\r')) {
+            textoLimpo += texto[i];
+        }
+    }
+  return textoLimpo;
+}
+
+
+// informar o numero do voo para outras funcoes
 void numCadastro(int &num){
   do{
     cout << "Informe o numero do voo:" << endl;
@@ -295,3 +437,74 @@ void numCadastro(int &num){
   while(num > TAM - 1 || num < 0);
 }
 
+
+//conta quantos voos tem preenchidos do vetor
+int contarVoos(Voo LA[]) {
+    int quantidade = 0;
+
+    for (int i = 0; i < TAM; i++) {
+        if (LA[i].num != 0) {
+            quantidade++;
+        }
+    }
+
+    return quantidade;
+}
+
+
+//escreve novamente o csv apos alguma alteracao em algum voo
+void atualizaArquivo(Voo LA[]){
+  string texto;
+  //caracteres
+  string aspa = "\"", csv = ",";
+  ofstream file("meusvoos.csv");
+
+  int id = contarVoos(LA);
+  if (file.is_open()) {
+        for (int i = 0; i < id; i++){
+          if(LA[i].num!= -1){
+            texto = aspa + to_string(LA[i].num) + aspa + csv + aspa + LA[i].origem + aspa + csv +
+            aspa + LA[i].destino + aspa + csv + aspa + to_string(LA[i].capacidade) + aspa + csv +
+            aspa + to_string(LA[i].qnt) + aspa + csv + aspa + to_string(LA[i].preco) + aspa;
+            file << texto << endl;
+          }         
+        }
+      file.close();
+  } else {
+      cout << "Erro ao abrir o arquivo." << endl;
+  }
+}
+
+
+// exclui um voo e atualiza no csv
+void deletarVoo(Voo LA[], int num){
+  char resposta;
+  for (int i = 0; i < TAM; i++) {
+    if (LA[i].num == num){
+      cout << "Tem certeza em deletar o voo n:"<< num << " ? [s/n]" << endl;
+      cin >> resposta;
+      if(resposta == 's'){
+        cout << "Voo deletado" << endl;
+        LA[i].num = -1;
+        // LA[i].origem = "";
+        // LA[i].destino = "";
+        // LA[i].capacidade = 0;
+        // LA[i].qnt = 0;
+        // LA[i].preco = 0;
+      }
+      else
+        cout << "Operacao abortada" << endl;
+      atualizaArquivo(LA);
+      carregaVetor(LA);
+      return;
+    }
+  }
+}
+
+
+// LA[i].num
+// LA[i].origem
+// LA[i].destino
+// LA[i].capacidade
+// LA[i].qnt
+// LA[i].preco
